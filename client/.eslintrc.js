ce6c26d875d1070062abd062d5216961446128ff
Nathan Thomas <nwthomas@me.com>
2019-10-10 00:07:07 -0700

    Upgrade eslint with a11y package

---
+++ client/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true
+  },
+  extends: [
+    `react-app`,
+    'airbnb',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/strict'
+  ],
+  plugins: ['import', 'react'],
+  rules: {
+    'valid-jsdoc': 2,
+    'react/jsx-uses-react': 1,
+    'react/jsx-no-undef': 2,
+    'react/jsx-wrap-multilines': 2,
+    'react/no-string-refs': 0
+  }
+};
