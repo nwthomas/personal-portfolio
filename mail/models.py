e296f0e99dae20f6a975696559195dc1f8e208e2
Nathan Thomas <nwthomas@me.com>
2019-10-10 22:03:58 -0700

    Make migrations and update schema.py

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Mail(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    subject = models.CharField(max_length=1000)
+    message = models.CharField(max_length=2000)
--- mail/schema.py
+++ mail/schema.py
@@ -1,11 +1,37 @@
-from django.db import models
-from uuid import uuid4
-from django.contrib.auth.models import User
+from django.conf import settings
+from graphene_django import DjangoObjectType
+import graphene
+from models import Mail
+from graphene_django.filter import DjangoFilterConnectionField
 
 
-class Mail(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=256)
-    email = models.CharField(max_length=256)
-    subject = models.CharField(max_length=1000)
-    message = models.CharField(max_length=2000)
+class MailType(DjangoObjectType):
+    class Meta:
+        model = Mail
+        interfaces = (graphene.relay.Node,)
+        # Filter fields must follow format of:
+        # filter_fields = ['content1', 'content2']
+        filter_fields = {
+            'name': ['exact', 'icontains', 'startswith'],
+            'email': ['exact', 'icontains', 'startswith'],
+            'subject': ['exact', 'icontains', 'startswith'],
+            'message': ['exact', 'icontains', 'startswith'],
+        }
+
+
+class Query(graphene.ObjectType):
+    mail = DjangoFilterConnectionField(MailType)
+
+
+class CreateMail(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        email = graphene.String()
+        subject = graphene.String()
+        message = models.String()
+        honey_field = models.String()
+    mail = graphene.Field(MailType)
+    ok = graphene.Boolean()
+
+    def mutate(self, info, title, context):
+        pass
