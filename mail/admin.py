3550ce9e65a4847b0b6782c5fa62b39e470f5e11
Nathan Thomas <nwthomas@me.com>
2019-10-10 21:44:14 -0700

    Update with Mail model and MailAdmin preferences

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Mail
 
-# Register your models here.
+
+class MailAdmin(admin.ModelAdmin):
+    readonly_fields = ["id"]
+
+
+admin.site.register(Mail, MailAdmin)
