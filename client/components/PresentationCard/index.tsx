eb95698f4d824cceb7b9d656d80c29996e44f6ac
Nathan Thomas <nwthomas@me.com>
2021-04-11 00:18:09 -0700

    Finish building default layout for presentation page

--- client/components/PresentationCard/index.tsx
+++ client/components/PresentationCard/index.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 import type { Presentation } from '../../staticAssets';
 
-interface Props extends Presentation {
-  // finish
-}
+// Renamed to Props for clarity in error messages
+type Props = Presentation;
 
 function PresentationCard({
   date,
@@ -28,9 +27,10 @@ function PresentationCard({
       </div>
       <div>
         <h4>{title}</h4>
-        <p>{`${meetupTitle} on ${date}`}</p>
-        <p>{presenters}</p>
-        <p>{location}</p>
+        <div>
+          <p>{presenters}</p>
+          <p>{`${meetupTitle} on ${date} at ${location}`}</p>
+        </div>
         <p>{description}</p>
       </div>
     </RootStyles>
@@ -38,13 +38,43 @@ function PresentationCard({
 }
 
 const RootStyles = styled.div`
-  align-self: flex-start;
+  flex-direction: column;
   display: flex;
   background-color: ${({ theme }) => theme.colors.bodyBackgroundAccentTwo};
   border: ${({ theme }) => `1px solid ${theme.colors.bodyBackgroundAccentOne}`};
   border-radius: ${({ theme }) => theme.borderRadii.small};
   padding: ${({ theme }) => theme.spaces.medium};
   width: 100%;
+
+  @media only screen and (min-width: ${({ theme }) =>
+      theme.breakpoints.desktop}) {
+    align-self: flex-start;
+    flex-direction: row;
+  }
+
+  > div:last-child {
+    margin-left: 0;
+    margin-top: ${({ theme }) => theme.spaces.medium};
+
+    @media only screen and (min-width: ${({ theme }) =>
+        theme.breakpoints.desktop}) {
+      margin-left: ${({ theme }) => theme.spaces.medium};
+      margin-top: 0;
+    }
+
+    > div {
+      margin: ${({ theme }) => theme.spaces.small} 0;
+
+      > p {
+        font-style: italic;
+        opacity: ${({ theme }) => theme.opacity.opacity60};
+      }
+    }
+
+    > p:last-child {
+      font-style: normal;
+    }
+  }
 `;
 
 export default PresentationCard;
--- client/staticAssets/presentations.ts
+++ client/staticAssets/presentations.ts
@@ -10,11 +10,11 @@ export interface Presentation {
 
 export const presentations: Array<Presentation> = [
   {
-    title: 'React Native Web for Twitter',
+    title: 'React Native Web at Twitter',
     meetupTitle: 'React Native at Shopify Meetup',
     presenters: 'Justine De Caires and Nathan Thomas',
     description:
-      "This talk was given at the Shopify HQ in San Francisco, CA following Shopify's big push towards using React Native throughout its product suite. Twitter uses React Native Web extensively, so it was a natural fit for us to present at it.",
+      "This talk was given at the Shopify HQ in San Francisco, CA following Shopify's big push towards using React Native throughout its product suite. Twitter uses React Native Web extensively, so it was a natural fit for us to present at it. We discussed the StyleSheet, cool primitives offered up by the library, and more.",
     date: 'January 29th, 2020',
     location: 'Shopify HQ, San Francisco, CA',
     url: 'https://www.youtube-nocookie.com/embed/GNrQTbIFsG4?start=2909',
