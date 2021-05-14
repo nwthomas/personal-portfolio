77f313bf86a4a4fae247a88d526c01b9c0b7ba90
Nathan Thomas <nwthomas@me.com>
2021-05-13 22:45:08 -0700

    Finalize server handlers for initial server

--- server/main.go
+++ server/main.go
@@ -9,20 +9,20 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
+
 	// This autoloads in the environment variables from a local .env file
 	_ "github.com/joho/godotenv/autoload"
 )
 
-
-type Response struct {
-	Success 	bool `json:"success"`
-	Message 	*Message `json:"message"`
-}
-
-type Message struct {
+type ResponseMessage struct {
 	StatusCode 	int `json:"status_code"`
 	Content 	string `json:"content"`
 	Error 		bool `json:"error"`
+}
+
+type Response struct {
+	Success 	bool `json:"success"`
+	Message 	*ResponseMessage `json:"message"`
 }
 
 type ReceivedEmail struct {
@@ -34,61 +34,53 @@ type ReceivedEmail struct {
 	Fax 		string `json:"fax,omitempty"`
 }
 
-type SentEmail struct {
-	From 		string `json:"from"`
-	To			string `json:"to"`
-	Subject		string `json:"subject"`
-	Text		string `json:"text"`
-}
-
 func sendEmail(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
+	fmt.Println("working")
 	var email ReceivedEmail
 	_ = json.NewDecoder(r.Body).Decode(&email)
 
 	if len(email.Fax) > 0 {
-		r := &Response{ Success: false, Message: &Message{ StatusCode: 404, Content: "Please try again", Error: true }}
+		r := &Response{ Success: false, Message: &ResponseMessage{ StatusCode: 404, Content: "Please try again", Error: true }}
+		
 		json.NewEncoder(w).Encode(r)
 	} else {
 		// A great guide for sending SMTP with Golang is at https://www.admfactory.com/send-emails-using-golang/
 		personalEmail := os.Getenv("EMAIL_PERSONAL_ADDRESS")
+		password := os.Getenv("EMAIL_PASSWORD")
 		from := personalEmail
 		to := []string {
 			personalEmail,
 		}
 
-		// username := os.Getenv("EMAIL_USERNAME")
-		password := os.Getenv("EMAIL_PASSWORD")
-
 		smtpHost := os.Getenv("EMAIL_HOST")
 		smtpPort := os.Getenv("EMAIL_PORT")
 
-		auth := smtp.PlainAuth("", personalEmail, password, smtpHost)
-
 		message := fmt.Sprintf("From: %s\r\n", personalEmail) +
-				fmt.Sprintf("To: %s\r\n", personalEmail) + 
-				fmt.Sprintf("Subject: %s\r\n", email.Subject) +
-				"\r\n" +
-				fmt.Sprintf("Email is from %s at %s\r\n\r\n%s\r\n", email.Name, email.Email, email.Message)
+		fmt.Sprintf("To: %s\r\n", personalEmail) + 
+		fmt.Sprintf("Subject: %s\r\n", email.Subject) +
+		"\r\n" +
+		fmt.Sprintf("Email is from %s at %s\r\n\r\n%s\r\n", email.Name, email.Email, email.Message)
 		
-		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(message))
+		auth := smtp.PlainAuth("", personalEmail, password, smtpHost)
+		smtpAddress := smtpHost + ":" + smtpPort
+		err := smtp.SendMail(smtpAddress, auth, from, to, []byte(message))
 
 		if err != nil {
 			fmt.Println(err)
 			return
+		} else {
+			r := &Response{ Success: true, Message: &ResponseMessage{ StatusCode: 201, Content: "Email sent successfully", Error: false } }
+			json.NewEncoder(w).Encode(r)
 		}
-
-		r := &Response{ Success: true, Message: &Message{ StatusCode: 201, Content: "Email sent successfully", Error: false } }
-		json.NewEncoder(w).Encode(r)
 	}
 }
 
 func main() {
-	r := mux.NewRouter()
-	
-	r.HandleFunc("/api/send-email", sendEmail).Methods("POST")
-	
 	port := os.Getenv("PORT")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
+	router := mux.NewRouter()
+	
+	router.HandleFunc("/api/send-email", sendEmail).Methods(http.MethodPost)
+	
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
