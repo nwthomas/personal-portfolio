64df105f00c2aff30be310bddcdde3180bbbeb35
Nathan Thomas <nwthomas@me.com>
2021-08-07 02:00:08 -0700

    Add small ipfs site for testing

---
+++ ipfs-client/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Nathan Thomas | Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <img
+          alt="Ethereum wallet address QR code"
+          draggable={false}
+          src="./wallet-address.png"
+        />
+      </main>
+    </div>
+  );
+}
