7f24d84729d97026bbf3afa5821c2d0cfcb640d0
Nathan Thomas <nwthomas@me.com>
2021-08-07 02:15:17 -0700

    Clean up linter and syntax for ipfs build

--- ipfs-client/next.config.js
+++ ipfs-client/next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   exportTrailingSlash: true,
-  exportPathMap: function () {
+  exportPathMap() {
     return {
-      "/": { page: "/" },
+      '/': { page: '/' },
     };
   },
 };
